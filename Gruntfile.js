7682a9d677e7e20c0a2c6e8d11ea950fe8aa9aa5
ItsEddy <yukkcake@gmail.com>
2016-01-09 04:46:30 +0800

    Setup closure-compiler for dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,45 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    'pkg': grunt.file.readJSON('package.json'),
-    'browserify': {
-        options: {
-            debug: true,
-            extensions: ['.js'],
-        },
-        dev: {
-            src: ['src/**/*.js'],
-            dest: 'dev/main.js',
-        },
-        dist: {
+    require('google-closure-compiler').grunt(grunt);
+    // Project configuration.
+    grunt.initConfig({
+        'pkg': grunt.file.readJSON('package.json'),
+        'browserify': {
             options: {
-                debug: false,
-                transform: ['uglify'],
+                debug: true,
+                extensions: ['.js'],
             },
-            src: '<%= browserify.dev.src %>',
-            dest: 'dist/main.js',
+            dev: {
+                src: ['src/**/*.js'],
+                dest: 'dev/main.js',
+            },
+            dist: {
+                options: {
+                    debug: false,
+                },
+                src: ['build/**/*.min.js'],
+                dest: 'dist/main.js',
+            },
         },
-    },
-    'uglify': {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-    },
-  });
+        'closure-compiler': {
+            dist: {
+                files: {
+                   'dist/main.min.js' : ['dist/main.js'],
+                },
+                options: {
+                    compilation_level: 'ADVANCED_OPTIMIZATIONS',
+                    language_in: 'ECMASCRIPT5_STRICT',
+                    create_source_map: 'dist/main.min.js.map',
+                    output_wrapper: '(function(){\n%output%\n}).call(this)\n//# sourceMappingURL=main.min.js.map',
+                },
+            }
+        },
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-browserify');
+    //grunt.loadNpmTasks('grunt-closure-compiler');
 
-  // Default task(s).
-  grunt.registerTask('default', ['browserify:dev']);
-  grunt.registerTask('dist', ['browserify:dist']);
+    // Default task(s).
+    grunt.registerTask('default', ['browserify:dev']);
+    grunt.registerTask('dist', ['browserify:dist','closure-compiler:dist']);
 
 };
