adb6b0b201d7dacdfc09d83ac8b3edc9e6bd8b77
Eddy Wu <yukkcake@gmail.com>
2016-01-29 04:04:43 +0800

    Add a minimal greasemonkey/tampermonkey loader

---
+++ eddy.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+//
+// @name         Eddy's plugin
+// @namespace    https://github.com/Eddy0402
+// @contributer  Eddy
+// @version      2.0.0
+// @description  Kiwi!
+// @author       Eddy0402
+// @license      GPLv3
+// @match        http://agar.io/*
+// @require      https://cdn.bootcss.com/jquery/1.11.3/jquery.min.js
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+/* Don't run on frames or iframes, so don't runs multiple times */
+if (window.top != window.self) {
+    return;
+}
+
+if(location.href.replace(location.hash,"") !== "http://agar.io/dd"){
+    location.href = "http://agar.io/dd";
+    return;
+}
+
+var GM = {
+    xmlhttpRequest : GM_xmlhttpRequest,
+};
+var Config = {
+    // change your own config when hosted by yourself
+    resource_base : 'http://agario.itseddy.me/',
+    server_url    : '127.0.0.1:8000',
+};
+
+unsafeWindow.$ = unsafeWindow.jQuery = jQuery;
+
+$.getScript('http://127.0.0.1:8080/main.js', function(){
+    unsafeWindow.LoadPlugin(GM, Config);
+});
